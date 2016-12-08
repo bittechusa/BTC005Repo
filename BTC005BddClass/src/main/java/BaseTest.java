da8f3de45d891c704e36b123e162b91abc78e4c3
russel <nhrussel10@gmail.com>
2016-12-07 20:51:10 -0500

    Moga 1

--- BTC005BddClass/src/main/java/BaseTest.java
+++ BTC005BddClass/src/main/java/BaseTest.java
@@ -7,15 +7,19 @@ public class BaseTest
 	
 	public HomePage startBrowser()
 	{
+		System.out.println("russel");
+		System.out.println("");
 		System.setProperty("webdriver.gecko.driver", "/Users/bittech/Downloads/geckodriver");
 		dr=new FirefoxDriver();
 		
 		dr.get("http://www.eshopper24.com");
 		return new HomePage(dr);
+	
 	}
 	public void end()
 	{
 		dr.quit();
+		System.out.println("chips");
 	}
 
 }
